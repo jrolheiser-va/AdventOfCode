242351626b14c15070735de9aaa7057fbd47df87
Jesse Rolheiser <jrolheiser@vendasta.com>
2017-12-03 22:19:40 -0600

    day 3 2017.

--- 2017/AdventOfCode.py
+++ 2017/AdventOfCode.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 get_input_for_day = lambda day: open('inputs/day_%s_input.txt' % str(day))
 part_2 = False
 
@@ -35,6 +37,48 @@ def day_2():
                         diffs.append(abs(max(nums[i], nums[j])/min(nums[i], nums[j])))
                         break
     return sum(diffs)
+
+
+def day_3():
+    move_right = lambda (x, y): (x + 1, y)
+    move_down = lambda (x, y): (x, y - 1)
+    move_left = lambda (x, y): (x - 1, y)
+    move_up = lambda (x, y): (x, y + 1)
+
+    moves = [move_right, move_up, move_left, move_down]
+
+    adjacent = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    def get_location_value(location, location_vals):
+        val = 0
+        for adj in adjacent:
+            val += location_vals[(location[0] + adj[0], location[1] + adj[1])]
+        return val
+
+    def gen_spiral(num):
+        curr_num = 1
+        location = (0, 0)
+        times_to_move = 1
+        curr_move = 0
+        location_values = defaultdict(lambda: 0)
+        location_values[location] = curr_num
+        while True:
+            for _ in range(2):
+                move = moves[curr_move]
+                for _ in range(times_to_move):
+                    if curr_num == num:
+                        return location
+                    location = move(location)
+                    curr_num += 1
+                    location_values[location] = get_location_value(location, location_values)
+                    if part_2 and location_values[location] > num:
+                        return location_values[location]
+                curr_move = (curr_move + 1) % 4
+            times_to_move += 1
+
+    input_num = 347991
+    result = gen_spiral(input_num)
+    return sum(map(abs, result)) if not part_2 else result
 
 
 if __name__ == '__main__':
